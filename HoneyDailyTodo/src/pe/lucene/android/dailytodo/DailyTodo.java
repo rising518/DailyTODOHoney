736f890c68b2b7f3352d1a03d58df55d1aba4929
Ddogos <lastforone@gmail.com>
2011-12-21 17:48:19 +0900

    commit 1

---
+++ HoneyDailyTodo/src/pe/lucene/android/dailytodo/DailyTodo.java
@@ -0,0 +1,13 @@
+package pe.lucene.android.dailytodo;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class DailyTodo extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
